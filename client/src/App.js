1aaabbd3188700e7995479508e4202ebb40bbf3a
Ivan Stelmakh <stelmahivan1994@gmail.com>
2019-04-17 16:00:41 +0300

    added components and finished file uploader

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './App.css';
 import { hot } from 'react-hot-loader/root';
 
-const App = () => <div>App</div>;
+import FileUpload from './components/FileUpload';
+
+const App = () => (
+  <div className="container mt-4">
+    <h4 className="display-4 text-center mb-4">
+      <i className="fab fa-react" /> React File Upload
+    </h4>
+
+    <FileUpload />
+  </div>
+);
 
 export default (process.env.NODE_ENV === 'development' ? hot(App) : App);
